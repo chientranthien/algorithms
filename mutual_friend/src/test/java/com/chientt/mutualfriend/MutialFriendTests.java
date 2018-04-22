46c20957db574085b52ebdb8be32285876010580
Chien <1112026@student.hcmus.edu.vn>
2018-04-22 14:13:57 +0700

    added more testcases

--- mutual_friend/src/test/java/com/chientt/mutualfriend/MutialFriendTests.java
+++ mutual_friend/src/test/java/com/chientt/mutualfriend/MutialFriendTests.java
@@ -5,6 +5,7 @@ import com.chientt.mutualfriend.model.User;
 import com.chientt.mutualfriend.service.impl.UserService;
 import com.chientt.mutualfriend.service.impl.UserServiceImpl;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.Test;
@@ -32,4 +33,57 @@ public class MutialFriendTests {
 
     }
 
+    @Test
+    public void testCase2() {
+        User user1 = new User();
+        user1.setFriends(Arrays.asList(1L, 2L, 3L, 4L));
+
+        User user2 = new User();
+        user2.setFriends(Arrays.asList(2L, 3L));
+
+        List<Long> mutualFriends = userService.findMutualFriends(user1, user2);
+        List<Long> expectation = Arrays.asList(2L, 3L);
+
+        assertThat(mutualFriends).isEqualTo(expectation);
+    }
+
+    @Test
+    public void testCase3() {
+        User user1 = new User();
+        user1.setFriends(Arrays.asList(3L, 4L, 5L, 6L));
+
+        User user2 = new User();
+        user2.setFriends(Arrays.asList(1L, 2L));
+
+        List<Long> mutualFriends = userService.findMutualFriends(user1, user2);
+
+        assertThat(mutualFriends).isEqualTo(Collections.EMPTY_LIST);
+    }
+
+    @Test
+    public void testCase4() {
+        User user1 = new User();
+        user1.setFriends(Arrays.asList(3L, 4L, 5L, 6L));
+
+        User user2 = new User();
+        user2.setFriends(Arrays.asList(7L, 8L));
+
+        List<Long> mutualFriends = userService.findMutualFriends(user1, user2);
+
+        assertThat(mutualFriends).isEqualTo(Collections.EMPTY_LIST);
+    }
+
+    @Test
+    public void testCase5() {
+        User user1 = new User();
+        user1.setFriends(Arrays.asList(3L, 4L, 5L, 6L));
+
+        User user2 = new User();
+        user2.setFriends(Arrays.asList(6L, 7L, 8L));
+
+        List<Long> mutualFriends = userService.findMutualFriends(user1, user2);
+        List<Long> expectation = Arrays.asList(6L);
+
+        assertThat(mutualFriends).isEqualTo(expectation);
+    }
 }
