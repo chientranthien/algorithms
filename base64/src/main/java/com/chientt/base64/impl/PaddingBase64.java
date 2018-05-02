6dbef155354cc41c3eea50f4b4b8abd702b1b554
Chien <1112026@student.hcmus.edu.vn>
2018-05-02 16:59:33 +0700

    refactor PaddingBase64

--- base64/src/main/java/com/chientt/base64/impl/PaddingBase64.java
+++ base64/src/main/java/com/chientt/base64/impl/PaddingBase64.java
@@ -9,6 +9,7 @@ import com.chientt.base64.Encoder;
 public class PaddingBase64 implements Encoder {
 
     static private final boolean DEBUG = true;
+    static private final byte ZERO_BYTE = 0;
     static private final byte PADDING_INDEX = 64;
 
     private static final char[] BASE_64_TABLE = {
@@ -30,51 +31,28 @@ public class PaddingBase64 implements Encoder {
         int resultLen = mod == 0 ? len * 4 / 3 : (len / 3 * 4) + 4;
         byte[] encodedBytes = new byte[resultLen];
         int k = 0;
-        for (int i = 0, j = 0; i < len - mod; i += 3) {
+        for (int i = 0, j = 0; i < len - mod; i += 3, k += 4) {
             byte b1 = strAsBytes[j++];
             byte b2 = strAsBytes[j++];
             byte b3 = strAsBytes[j++];
-
-            byte encoded1 = (byte) (b1 >> 2);
-            byte residual1 = (byte) (b1 & 0x03);
-            byte encoded2 = (byte) ((b2 >> 4) | (residual1 << 4));
-            byte residual2 = (byte) ((b2 & 0x0f) << 2);
-            byte encoded3 = (byte) (residual2 | (b3 >> 6));
-            byte encoded4 = (byte) ((b3 & 0x3f));
-
-            if (DEBUG) {
-                System.out.println("b1: " + b1 + " b2: " + b2 + " b3: " + b3);
-                System.out.println("c1: " + encoded1 + " c2: " + encoded2
-                        + " c3: " + encoded3 + " c4: " + encoded4);
-            }
-
-            encodedBytes[k++] = encoded1;
-            encodedBytes[k++] = encoded2;
-            encodedBytes[k++] = encoded3;
-            encodedBytes[k++] = encoded4;
+            encodeTriple(b1, b2, b3, encodedBytes, k);
         }
         if (mod == 1) {
-            byte b1 = strAsBytes[len - 2];
-            byte residual = (byte) (b1 & 0x03);
-
-            encodedBytes[k++] = (byte) (b1 >> 2);
-            encodedBytes[k++] = (byte) (residual << 4);
+            byte b1 = strAsBytes[len - 1];
+            encodedBytes[k++] = get1stEncodedByte(b1);
+            encodedBytes[k++] = get2ndEncodedByte2(b1, ZERO_BYTE);
             encodedBytes[k++] = PADDING_INDEX;
-            encodedBytes[k++] = PADDING_INDEX;
+            encodedBytes[k] = PADDING_INDEX;
         } else if (mod == 2) {
             byte b1 = strAsBytes[len - 2];
-            byte residual = (byte) (b1 & 0x03);
             byte b2 = strAsBytes[len - 1];
-            encodedBytes[k++] = (byte) (b1 >> 2);
-            encodedBytes[k++] = (byte) (residual << 4 | b2 >> 4);
-            encodedBytes[k++] = (byte) ((b2 & 0x0f) << 2);
-            encodedBytes[k++] = PADDING_INDEX;
+            encodedBytes[k++] = get1stEncodedByte(b1);
+            encodedBytes[k++] = get2ndEncodedByte2(b1, b2);
+            encodedBytes[k++] = get3rdEncodedByte(b2, ZERO_BYTE);
+            encodedBytes[k] = PADDING_INDEX;
         }
         char[] encodedChar = new char[encodedBytes.length];
         for (int i = 0; i < encodedChar.length; i++) {
-            if (encodedBytes[i] == -32) {
-                System.out.println("encodedBytes[i]=-32 i:" + i);
-            }
             encodedChar[i] = BASE_64_TABLE[encodedBytes[i]];
         }
         return new String(encodedChar);
@@ -84,6 +62,32 @@ public class PaddingBase64 implements Encoder {
     public String decode(String str) {
         return null;
 
+    }
+
+    private byte get1stEncodedByte(byte b1) {
+        return (byte) (b1 >> 2);
+    }
+
+    private byte get2ndEncodedByte2(byte b1, byte b2) {
+        byte residual1 = (byte) (b1 & 0x03);
+        return (byte) ((b2 >> 4) | (residual1 << 4));
+    }
+
+    private byte get3rdEncodedByte(byte b2, byte b3) {
+        byte residual2 = (byte) ((b2 & 0x0f) << 2);
+        return (byte) (residual2 | (b3 >> 6));
+    }
+
+    private byte get4thEncodedByte(byte b3) {
+        return (byte) ((b3 & 0x3f));
+    }
+
+    private void encodeTriple(byte b1, byte b2, byte b3, byte[] encodedBytes, int fromIndex) {
+
+        encodedBytes[fromIndex++] = get1stEncodedByte(b1);
+        encodedBytes[fromIndex++] = get2ndEncodedByte2(b1, b2);
+        encodedBytes[fromIndex++] = get3rdEncodedByte(b2, b3);
+        encodedBytes[fromIndex] = get4thEncodedByte(b3);
     }
 
 }
