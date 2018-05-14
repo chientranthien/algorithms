174776bcadf9e2dc7b7991723b187b26fe4f2b0e
Chien <1112026@student.hcmus.edu.vn>
2018-05-14 17:07:16 +0700

    coded leftrotate function

--- md5/src/main/java/com/chientt/md5/MD5Encoder.java
+++ md5/src/main/java/com/chientt/md5/MD5Encoder.java
@@ -7,21 +7,29 @@ public class MD5Encoder implements Hash {
 
     public static final int BIT_PER_BYTE = 8;
 
-    public static final int[] ALL_ROUND = new int[]{
-            7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-            5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
-            4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-            6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+    public static final int[] s = new int[]{
+        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+        5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
 
-    public static final int[] K = new int[64];
-
-    static {
-        for (int i = 0; i < 64; i++) {
-            K[i] = (int) Math.floor(
-                    Math.pow(2, 32)
-                            * Math.abs(Math.sin(i + 1)));
-        }
-    }
+    public static final int[] K = new int[]{
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
     public static byte ZERO = 0x00;
     public static byte FIRT_BIT_1 = (byte) 0x80;
@@ -42,15 +50,15 @@ public class MD5Encoder implements Hash {
         } else {
             paddingLen = a + 512;
         }
-        byte bit1 = (byte) 0x80;
 
-        byte[] padding = new byte[paddingLen / 8];
+        int paddingAsByteLen = paddingLen / 8;
+        byte[] padding = new byte[paddingAsByteLen];
         padding[0] = (byte) 0x80;
-        for (int i = 1; i < paddingLen - 8; i++) {
+        for (int i = 1; i < paddingAsByteLen - 8; i++) {
             padding[i] = 0;
         }
         byte[] bitCountAsBytes = convertLongToBytes(bitCount);
-        System.arraycopy(bitCountAsBytes, 0, padding, paddingLen - 8, 8);
+        System.arraycopy(bitCountAsBytes, 0, padding, paddingAsByteLen - 8, 8);
         byte[] newArr = new byte[padding.length + bytes.length];
 
         System.arraycopy(bytes, 0, newArr, 0, bytes.length);
@@ -79,14 +87,27 @@ public class MD5Encoder implements Hash {
                     g = (7 * i) % 16;
                 }
                 F += A + K[i] + M[g];
-                A=D;
-                D=C;
-                C=B;
-                
-            }
+                A = D;
+                D = C;
+                C = B;
+                B += leftrotate(F, s[i]);
 
+            }
+            a0 += a0 + A;
+            b0 += b0 + B;
+            c0 += c0 + C;
+            d0 += d0 + D;
         }
+
+        System.out.println("a0 " + Integer.toHexString(a0));
+        System.out.println("b0 " + Integer.toHexString(b0));
+        System.out.println("c0 " + Integer.toHexString(c0));
+        System.out.println("d0 " + Integer.toHexString(d0));
         return null;
+    }
+
+    private int leftrotate(int x, int c) {
+        return (x << c) | (x >> (32 - c));
     }
 
     @Override
@@ -118,7 +139,7 @@ public class MD5Encoder implements Hash {
             num &= input[i++] << 16;
             num &= input[i++] << 8;
             num &= input[i++];
-            result[i] = num;
+            result[j] = num;
         }
         return result;
     }
--- md5/src/test/java/com/chientt/md5/MyTest.java
+++ md5/src/test/java/com/chientt/md5/MyTest.java
@@ -1,15 +1,14 @@
 package com.chientt.md5;
 
+import java.util.Base64;
+
 /**
  * @author chientt
  */
 public class MyTest {
 
     public static void main(String[] args) {
-        byte a = 1;
-        int b = a << 30;
-        System.out.println(a);
-
-        System.out.println(b);
+        Hash h = new MD5Encoder();
+        h.hash("a");
     }
 }
