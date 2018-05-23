00b6ad9c03031b6c3c1d4d846c4b0612b8f9ca6f
chien tran thien <vinadonquixote@gmail.com>
2018-05-23 20:06:24 +0700

    added comment

--- insertion_sort/src/main/java/com/chientt/sort/InsertionSort.java
+++ insertion_sort/src/main/java/com/chientt/sort/InsertionSort.java
@@ -7,7 +7,7 @@ public class InsertionSort<T> implements ArraySort<T> {
     @Override
     public T[] sort(T[] input, Comparator comparator) {
         T[] result = Arrays.copyOf(input, input.length);
-
+//test
         for (int i = 1; i < result.length; i++) {
             T value = result[i];
             for (int j = i - 1; j >= 0; j--) {
