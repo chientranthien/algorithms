0fd7bcf595e2ae197ad96b0f76f73bc1ebde0fa8
Chien <1112026@student.hcmus.edu.vn>
2018-05-25 17:36:51 +0700

    completed quick sort

--- quick_sort/src/main/java/com/chientt/sort/QuickSort.java
+++ quick_sort/src/main/java/com/chientt/sort/QuickSort.java
@@ -11,21 +11,21 @@ public class QuickSort<T> implements ArraySort<T> {
     @Override
     public T[] sort(T[] input, Comparator<T> comparator) {
         T[] result = Arrays.copyOf(input, input.length);
-        int head = 0;
-        int tail = 0;
-        int pivot = (head + tail) / 2;
-
+        quickSort(result, 0, input.length - 1, comparator);
         return result;
     }
 
-    private void sort(T[] input,
-            int tail,
-            int pivot,
+    private void quickSort(T[] input,
             int head,
+            int tail,
             Comparator<T> comparator) {
-        for (int i = pivot; i >= tail; i--) {
-
+        pivot(input, head, tail, comparator);
+        if (tail <= head) {
+            return;
         }
+        int med = (head + tail) / 2;
+        quickSort(input, head, med, comparator);
+        quickSort(input, med + 1, tail, comparator);
     }
 
     public void pivot(T[] input, int index1, int index2, Comparator<T> comparator) {
@@ -33,12 +33,14 @@ public class QuickSort<T> implements ArraySort<T> {
         T pivotVal = input[pivotIndex];
         int i = pivotIndex;
         while (i >= index1) {
+
             if (comparator.compare(pivotVal, input[i])) {
-                for (int j = pivotIndex - 1; j >= i; j--) {
-                    input[j+1] = input[j];
+                T tmp = input[i];
+                for (int j = i; j < pivotIndex; j++) {
+                    input[j] = input[j + 1];
                 }
-                pivotIndex = i;
-                input[pivotIndex] = pivotVal;
+                input[pivotIndex] = tmp;
+                pivotIndex--;
             }
             i--;
         }
--- quick_sort/src/test/java/com/chientt/sort/QuickSortTests.java
+++ quick_sort/src/test/java/com/chientt/sort/QuickSortTests.java
@@ -5,39 +5,41 @@ import org.junit.Test;
 
 public class QuickSortTests {
 
-//    @Test
-//    public void shouldSortAsc() {
-//        Integer input[] = new Integer[]{3, 1, 6, 17, 5, 1};
-//        Integer expectation[] = new Integer[]{1, 1, 3, 5, 6, 17};
-//        ArraySort<Integer> arraySort = new QuickSort();
-//        Integer[] result = arraySort.sort(
-//                input,
-//                (a, b) -> a < b);
-//
-//        Assertions.assertThat(result).isEqualTo(expectation);
-//    }
-//
-//    @Test
-//    public void shouldSortDesc() {
-//        Integer input[] = new Integer[]{3, 1, 6, 17, 5, 1};
-//        Integer expectation[] = new Integer[]{17, 6, 5, 3, 1, 1};
-//        ArraySort<Integer> arraySort = new QuickSort();
-//        Integer[] result = arraySort.sort(
-//                input,
-//                (a, b) -> a > b);
-//
-//        Assertions.assertThat(result).isEqualTo(expectation);
-//    }
     @Test
-    public void testPivot() {
+    public void shouldSortAsc() {
         Integer input[] = new Integer[]{3, 1, 6, 17, 5, 1};
-        QuickSort<Integer> arraySort = new QuickSort();
+        Integer expectation[] = new Integer[]{1, 1, 3, 5, 6, 17};
+        ArraySort<Integer> arraySort = new QuickSort();
+        Integer[] result = arraySort.sort(
+                input,
+                (a, b) -> a < b);
 
-        arraySort.pivot(input, 0, input.length - 1, (a, b) -> a < b);
-
-        for (Integer integer : input) {
-            System.out.println(integer);
-        }
-
+        Assertions.assertThat(result).isEqualTo(expectation);
     }
+
+    @Test
+    public void shouldSortDesc() {
+        Integer input[] = new Integer[]{3, 1, 6, 17, 5, 1};
+        Integer expectation[] = new Integer[]{17, 6, 5, 3, 1, 1};
+        ArraySort<Integer> arraySort = new QuickSort();
+        Integer[] result = arraySort.sort(
+                input,
+                (a, b) -> a > b);
+
+        Assertions.assertThat(result).isEqualTo(expectation);
+    }
+    
+    
+//    @Test
+//    public void testPivot() {
+//        Integer input[] = new Integer[]{3, 1, 6, 17, 2, 4};
+//        QuickSort<Integer> arraySort = new QuickSort();
+//
+////        arraySort.pivot(input, 0, input.length - 1, (a, b) -> a < b);
+//        arraySort.sort(input, (a, b) -> a < b);
+//        for (Integer integer : input) {
+//            System.out.println(integer);
+//        }
+//
+//    }
 }
