5748817802f11a472087ec454c2c62a61ecfd3b6
Chien <1112026@student.hcmus.edu.vn>
2018-05-29 17:54:30 +0700

    init project merge sort

---
+++ merge_sort/src/main/java/com/chientt/sort/Comparator.java
@@ -0,0 +1,5 @@
+package com.chientt.sort;
+
+    public interface Comparator<T> {
+    boolean compare(T value1, T value2);
+}
---
+++ merge_sort/src/main/java/com/chientt/sort/MergeSort.java
@@ -0,0 +1,14 @@
+package com.chientt.sort;
+
+import java.util.Arrays;
+
+public class MergeSort<T> implements ArraySort<T> {
+aaaaaaaaaa
+    @Override
+    public T[] sort(T[] input, Comparator<T> comparator) {
+
+    }
+
+
+
+}
